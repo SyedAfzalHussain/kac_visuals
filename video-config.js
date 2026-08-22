66d0c7cacebf88dacf1bdc6d530a6da38872a019
Afzal <kazmiafzal348@gmail.com>
2026-08-22 23:32:51 +0500

    updated videos diff cloudinary

--- video-config.js
+++ video-config.js
@@ -1,13 +1,13 @@
 window.KARRAR_VIDEOS = {
-  hero: 'https://res.cloudinary.com/dluwhhwoi/video/upload/f_mp4,q_auto:good,w_1600,c_limit/v1785944894/KVS_pm9inp.mp4',
-  footer: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785870139/cine-26-844_Duplex_Avenue_HOR_V3_opglit.mp4',
-  portrait1: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785861561/clu-006-100_Palm_Hill_LN-001_2_1_of2vzb.mp4',
-  portrait2: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785970447/Cl-Richard-002_cn7033.mp4',//
-  portrait3: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785970419/keb-35-ally_defcan.mp4',// listing video with agent
-  portrait4: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785970395/edgar-ROB_d8uld0.mp4',//simple branding edit 
-  portrait5: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785861601/9Kona_realty_Gym_wtuah2.mp4',//comercial edit
+  hero: 'https://res.cloudinary.com/iwykyhoo/video/upload/f_mp4,q_auto:good,w_1600,c_limit/v1787409201/KVS.mp4',
+  footer: 'https://res.cloudinary.com/iwykyhoo/video/upload/f_mp4,q_auto:good,w_1600,c_limit/v1787409201/KVS.mp4',
+  portrait1: 'https://res.cloudinary.com/dewo6btsc/video/upload/clu-006-100_Palm_Hill_LN-001_2_1.mp4',
+  portrait2: 'https://res.cloudinary.com/dnkh1rvdr/video/upload/MM_srq-03-fitness_signature_spaces_v2_video_1_1_dk0idd.mp4',//
+  portrait3: 'https://res.cloudinary.com/moqokijq/video/upload/clu-009-5140_Miriam_St_with_logo.mp4',// listing video with agent
+  portrait4: 'https://res.cloudinary.com/moqokijq/video/upload/v1787416535/edgar-ROB.mp4',//simple branding edit 
+  portrait5: 'https://res.cloudinary.com/s1t1gqaw/video/upload/v1787420605/9Kona_realty_Gym.mp4',//comercial edit
   portrait6: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785861517/SDF_new_version_1_dzxj5w.mp4',
-  portrait7: 'https://res.cloudinary.com/dluwhhwoi/video/upload/v1785861545/2018_princess_Y75_hype_reel_V4_gwobcj.mp4',
-  landscape1: 'https://res.cloudinary.com/dluwhhwoi/video/upload/f_mp4,vc_h264,q_auto:good,w_1280,c_limit/v1785970460/cine-025-3301-16_Brookers_Lane_UNBRAND_inu9aq.mp4',
-  landscape2: 'https://res.cloudinary.com/dluwhhwoi/video/upload/f_mp4,q_auto:good,w_1600,c_limit/v1785944894/KVS_pm9inp.mp4'
+  portrait7: 'https://res.cloudinary.com/dtmo43sox/video/upload/v1787420324/2018_princess_Y75_hype_reel_V4_s8mkxn.mp4',
+  landscape1: 'https://res.cloudinary.com/dijdzmfvn/video/upload/f_mp4,vc_h264,q_auto:good,w_1280,c_limit/cine-018-16_tranby_avenue_annex_2_wavjfu.mp4',
+  landscape2: 'https://res.cloudinary.com/iwykyhoo/video/upload/f_mp4,q_auto:good,w_1600,c_limit/v1787409201/KVS.mp4'
 };
